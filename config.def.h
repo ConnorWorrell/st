a4249d997cca66d5f87268b941909c70147a9fa1
Connor Worrell <chemicalcoal1@gmail.com>
2022-02-19 21:54:12 -0700

    minor font change

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
-static char *font = "Inconsolata:pixelsize=22:antialias=true:autohint=true:spacing=20";
+static char *font = "Inconsolata:pixelsize=20:antialias=true:autohint=true:spacing=20";
 static int borderpx = 2;
 
 /*
